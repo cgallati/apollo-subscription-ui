ed33ae152e00c01aba6cc45781ccb7acba1e9c40
Eric Karnis <eric@karnis.ca>
2022-05-13 11:09:53 -0400

    readded modal

--- components/stages/AddStory.js
+++ components/stages/AddStory.js
@@ -1,6 +1,5 @@
 import { Button, Layout, Text, TextField } from "@audi/audi-ui-react"
 import styled from 'styled-components'
-import { useState } from "react"
 
 const BigButton = styled(Button)`
     width: 300px;
@@ -8,39 +7,14 @@ const BigButton = styled(Button)`
 
 const NoStoriesContainer = styled(Layout)`
     width: 100%;
-    height: 100%;
-`
+    height: 50vh;
+    `
 
-const WideInput = styled(TextField)`
-    width: 300px;
-`
-
-export default function NoStories({roomName}) {
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-  
+export default function AddStory({roomName, openModal}) {
     return(
         <NoStoriesContainer justify='center' align='center' direction='column'>
           <Text variant="order1" weight="bold" spaceStackEnd="xl">{roomName} Room</Text>
-          <WideInput 
-                label="Story Name" 
-                inputId='story-name-input' 
-                minlength='2' 
-                spaceStackEnd="l" 
-                value={name}
-                onChange={(e=>setName(e.target.value))}
-                required 
-            />
-            <WideInput 
-                label="Description" 
-                inputId='story-description-input' 
-                minlength='2' 
-                spaceStackEnd="xxl" 
-                value={description}
-                onChange={(e=>setDescription(e.target.value))}
-                required 
-            />
-          <BigButton variant='primary' onClick={()=>setShowVote(true)}>
+          <BigButton variant='primary' onClick={openModal}>
               Add Story
             </BigButton>
         </NoStoriesContainer>
